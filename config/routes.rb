78b26a80dbe414ca83f14154e0935e80b9d5b152
tomoprogrammer <train.vegacat@gmail.com>
2021-09-02 14:21:55 +0000

    Inital Commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+  root to: 'homes#top'
+
+  get 'home/about' => 'homes#about'
+
+  devise_for :users
+
+  resources :books
+
+  resources :users, only: [:show,:index,:edit,:update]
+
+
+
+
+
+
+
+end
